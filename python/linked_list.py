b6193db7839d172b7408f7a00cb51f3d26f3d77d
Balaji <hackwithbalaji@gmail.com>
2023-02-20 17:14:20 +0530

    removeNthFromEnd

--- python/linked_list.py
+++ python/linked_list.py
@@ -1,9 +1,49 @@
+import math
+
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
+def find_median(linked_list):
+    mapper = {}
+    count = 0
+    temp = linked_list
+    while temp != None:
+        count += 1
+        mapper[count] = temp.data
+        temp = temp.next
+    
+    return mapper[math.ceil(count/2)]
 
+def removeNthFromEnd(head, n):
+    count = 0
+    temp = head 
+    while temp != None:
+        count += 1
+        temp = temp.next
+
+    index = count - (n-1)
+
+    if index == 1:
+        node_to_remove = head
+        head = head.next
+        del node_to_remove
+        return head
+
+    count = 1
+    temp = head
+    while count != index-1:
+        count += 1
+        temp = temp.next
+    
+    node_to_remove = temp.next
+    temp.next = node_to_remove.next
+    node_to_remove.next = None
+
+    return head
+
+        
 
 head = Node(1)
 
@@ -17,4 +57,7 @@ printNode = head
 while printNode != None:
     print(printNode.data)
     printNode = printNode.next
+
+print("MEDIAN IS ", find_median(head))
+removeNthFromEnd(head, 2)
 
